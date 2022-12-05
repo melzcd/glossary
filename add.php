ae72b346405633555570b45fc2e9a603659f741b
melzcd <devilzcd@gmail.com>
2022-12-05 12:55:56 +0300

    Update add.php

--- add.php
+++ add.php
@@ -149,7 +149,7 @@ if (isset($_POST['saveGlossary']))
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Судебный департамент 2015</p>
+                    <p>Copyright &copy;2015</p>
                 </div>
             </div>
         </footer>
